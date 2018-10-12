d9e50a27ac59deca07c33fe371a301f69955e0eb
Alexander Olmedo <olmedo2408@gmail.com>
2018-10-12 17:07:10 -0500

    Variables

---
+++ 01-javascript/01-variables.js
@@ -0,0 +1,12 @@
+// NO TIPADO
+var edad = 10;
+var edadString = "10";
+var sueldo = 1.234;
+var casado = false;
+var hijos = null;
+
+console.log("edad", typeof edad); // number
+console.log("edadString", typeof edadString); // string
+console.log("sueldo", typeof sueldo); // number
+console.log("casado", typeof casado); // boolean
+console.log("hijos", typeof hijos); // boolean
