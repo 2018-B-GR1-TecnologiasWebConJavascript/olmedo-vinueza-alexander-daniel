f17254ba1d4b2abf67d2edbd29dac5da6b97490a
Alexander Olmedo <olmedo2408@gmail.com>
2018-10-15 17:03:06 -0500

    Terminar arreglos

--- 01-javascript/02-arreglos.js
+++ 01-javascript/02-arreglos.js
@@ -39,9 +39,31 @@ console.log(arregloNumeros);
 
 var arregloUno = arregloNumeros.slice(0, 3);
 var arregloDos = arregloNumeros.slice(3, 7);
-arregloDos.push(7);
-var indiceSiete = arregloDos.indexOf(7);
-console.log(arregloUno);
-console.log(arregloDos);
-console.log(indiceSiete);
+
+var arregloUnoDos = [1, 2];
+var arregloSeis = [6];
+
+// Destructutacion de arreglos
+
+console.log(...arregloUnoDos);
+console.log(1, 2);
+
+var arregloTotal = [...arregloUnoDos,
+    ...arregloUno,
+    ...arregloSeis,
+    ...arregloDos];
+
+console.log(arregloTotal);
+
+var arregloSiguientesNumeros = [11, 12, 13.14, 15, 16, 17, 18, 19, 20]
+
+arregloTotal.splice(arregloTotal.length, 0, ...arregloSiguientesNumeros)
+
+console.log(arregloTotal);
+
+// arregloDos.push(7);
+// var indiceSiete = arregloDos.indexOf(7);
+// console.log(arregloUno);
+// console.log(arregloDos);
+// console.log(indiceSiete);
 
