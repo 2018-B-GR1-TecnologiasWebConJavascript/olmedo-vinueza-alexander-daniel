9097a89912f768f2a4db3dfd7337cc1f96da35fc
Alexander Olmedo <olmedo2408@gmail.com>
2018-11-09 16:39:49 -0500

    Pipes

--- 02-typescript/02-observables.ts
+++ 02-typescript/02-observables.ts
@@ -1,13 +1,29 @@
+//import {map} from "rxjs/operators";
 
 declare var require: any;
 
 const rxjs = require('rxjs');
+const map = require('rxjs/operators').map;
 
-const numeros$ = rxjs.of(1,2,3,4,5,6);
+const numeros$ = rxjs.of(
+    1,
+    "Alexander",
+    true,
+    {nombre: 'Alexander'},
+    [1,2,3],
+    new Date);
 
 console.log(numeros$);
 
 numeros$
+    .pipe(
+        map((valorActual)=>{
+                return {
+                    data: valorActual
+                };
+            }
+        )
+    )
     .subscribe(
         (ok)=>{
             console.log('En OK', ok);
