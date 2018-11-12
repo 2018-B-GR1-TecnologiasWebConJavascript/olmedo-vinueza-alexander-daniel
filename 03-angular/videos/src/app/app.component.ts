f839ed192de82ea817f97afd15d87e709f1cb938
Alexander Olmedo <olmedo2408@gmail.com>
2018-11-12 17:56:00 -0500

    Angular

---
+++ 03-angular/videos/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {} from 'http-server';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'videos';
+}
