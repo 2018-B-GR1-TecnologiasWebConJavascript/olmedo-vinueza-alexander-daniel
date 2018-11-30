fa04ff434b2df12bdf4db22d3e8fc87e08f172ad
Alexander Olmedo <olmedo2408@gmail.com>
2018-11-30 17:49:20 -0500

    ejercicio 11

--- examen-01/pokemon/examen.ts
+++ examen-01/pokemon/examen.ts
@@ -40,3 +40,93 @@ arregloRespuestaMove = data
 
 console.log(arregloRespuestaMove);
 
+//11) Realizar la operacion de crear nuevos pokemons con la libreria inquirer.js
+
+const inquirer = require('inquirer');
+
+var preguntas = [
+    {
+        type: 'list',
+        name: 'abilities',
+        message: 'Seleccione la habilidad',
+        choices: ['comer', 'dormir'],
+    },
+    {
+        type: 'input',
+        name: 'base_experience',
+        message: "Cuál es la experiencia base",
+        default: 64
+    },
+    {
+        type: 'input',
+        name: 'game_indices',
+        message: "Cuál es el índice de juego",
+        default: 124
+    },
+    {
+        type: 'input',
+        name: 'height',
+        message: "Cuál es la estatura",
+        default: 12
+    },
+    {
+        type: 'list',
+        name: 'held_items',
+        message: 'Seleccione los items',
+        choices: ['manzanas', 'peras'],
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Ingrese el id",
+        default: '1'
+    },
+    {
+        type: 'confirm',
+        name: 'is_default',
+        message: '¿Es default?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'location_area_encounters',
+        message: "Ingrese la localización de encuentros",
+        default: 'Quito'
+    },
+    {
+        type: 'list',
+        name: 'moves',
+        message: 'Seleccione los movimientos',
+        choices: ['cut', 'jump'],
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: "Ingrese el nombre del pokemon",
+        default: 'pikachu'
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: "Ingrese el orden",
+        default: 233
+    },
+    {
+        type: 'list',
+        name: 'types',
+        message: 'Seleccione el tipo',
+        choices: ['grass', 'water'],
+    },
+    {
+        type: 'input',
+        name: 'weight',
+        message: "Cuál es el peso",
+        default: 64
+    }
+];
+
+inquirer.prompt(preguntas).then(respuestas => {
+    console.log('\nPokemon:');
+    console.log(JSON.stringify(respuestas, null, '  '));
+});
+
