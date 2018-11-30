829e08b8c2830be71a9580bccc1c997fa5b6ce70
Alexander Olmedo <olmedo2408@gmail.com>
2018-11-30 16:58:38 -0500

    ejercicio 2

--- examen-01/pokemon/examen.ts
+++ examen-01/pokemon/examen.ts
@@ -1,19 +1,29 @@
 const data = require('./data.json');
 
-var arregloRespuestaTypes = [];
-
-
 //1) Busque los tipos de "types" en el arreglo data.json
 
-const distinct = (value,index,self) => {
-    return self.indexOf(value) === index;
-};
+var arregloRespuestaTypes = [];
 
 arregloRespuestaTypes = data
     .map((pokemon)=>{
         return pokemon.types.map((types)=>{
             return types.type
         });
-    }).filter(distinct);
+    });
 
 console.log(arregloRespuestaTypes);
+
+//2) Busque los tipos de "abilities" en el arreglo data.json
+
+var arregloRespuestaAbilities = [];
+
+arregloRespuestaAbilities = data
+    .map((pokemon)=>{
+        return pokemon.abilities.map((abilities)=>{
+            return abilities.ability
+        });
+    });
+
+console.log(arregloRespuestaAbilities);
+
+
