cd565f38bb583d83de5cdb63755232e86dac2156
Alexander Olmedo <olmedo2408@gmail.com>
2018-12-10 17:59:15 -0500

    Interpolaciòn, directivas, event binding

--- 03-angular/videos/src/app/rutas/ruta-gestion-usuarios/ruta-gestion-usuarios.component.ts
+++ 03-angular/videos/src/app/rutas/ruta-gestion-usuarios/ruta-gestion-usuarios.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RutaGestionUsuariosComponent implements OnInit {
 
+  usuarios: Usuario[] = [
+    {
+      id: 1,
+      nombre: 'Alexander'
+    },
+    {
+      id: 2,
+      nombre: 'Daniel'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  hola(){
+    return 'Hola';
+  }
+
+  imprimir(usuario:Usuario){
+    console.log('Imprimir', usuario)
+    const indiceUsuarioAEliminar = this.usuarios.findIndex(
+      (usuarioABuscar)=>{
+      return usuarioABuscar.id == usuario.id
+    });
+    this.usuarios.splice(indiceUsuarioAEliminar, 1)
+  }
+
+}
+
+interface Usuario {
+  nombre?:string;
+  id?: number;
 }
