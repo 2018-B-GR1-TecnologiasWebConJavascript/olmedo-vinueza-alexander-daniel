2b9189ede750d596710cf1116ea90a614f6309f2
Alexander Olmedo <olmedo2408@gmail.com>
2019-01-28 17:36:25 -0500

    Send data continuously

--- 05-Raspberry/raspberry/config/bootstrap.js
+++ 05-Raspberry/raspberry/config/bootstrap.js
@@ -8,6 +8,7 @@
  * For more information on seeding your app with fake data, check out:
  * https://sailsjs.com/config/bootstrap
  */
+const axios = require('axios');
 
 module.exports.bootstrap = async function () {
 
@@ -17,9 +18,23 @@ module.exports.bootstrap = async function () {
 
 function emitirTemperatura() {
   setInterval(
-    () => {
+    async () => {
       const temperatura = new Date().toTimeString();
-      console.log(temperatura);
+      // Guardar la temperatura en la BDD
+      // REST -> POST
+      const url = 'http://localhost:1337/Temperatura'
+      try {
+        const respuesta = await axios({
+            method: 'post',
+            url: url,
+            data: {
+              medicion: temperatura
+            }
+          });
+        console.log(respuesta.data);
+      } catch (e) {
+        console.error(e);
+      }
     },
     2000
   )
