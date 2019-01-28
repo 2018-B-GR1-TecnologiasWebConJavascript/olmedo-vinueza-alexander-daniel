62b112ff8b40c89723f97507502f2cd0b3d25e61
Alexander Olmedo <olmedo2408@gmail.com>
2019-01-28 16:53:40 -0500

    Raspberry

---
+++ 04-backend/video-backend/api/controllers/RaspberryController.js
@@ -0,0 +1,30 @@
+/**
+ * RaspberryController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+const axios = require('axios');
+
+module.exports = {
+
+  saludo: async (req, res) => {
+    const parametros = req.allParams();
+    const url = 'http://localhost:1338/board/saludo'
+    const saludo = {
+      saludo: parametros.saludo
+    };
+    try {
+      const respuestaRaspberry = await axios.post(url.saludo);
+      return res.ok(respuestaRaspberry);
+    } catch (e) {
+      return res.serverError({
+        error: 500,
+        mensaje: 'error en el servidor'
+      });
+    }
+  }
+
+};
+
